3dab3887f8a5c77e24bc5f432e2baa7c49571c00
root <violetacastrogutierrez@gmail.com>
2020-11-18 00:12:33 +0000

    styling button and wish list cant be empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addItem, deleteItem } from './redux/actions';
+import { ListGroup, Card, Container, Button } from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { input: ""  };
+    this.state = { input: "" };
     this.handleAddItem = this.handleAddItem.bind(this);
     this.handleDeleteItem = this.handleDeleteItem.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   updateInput(input) {
@@ -15,7 +18,25 @@ class App extends React.Component {
   }
 
   handleAddItem() {
-    this.props.addItem(this.state.input)
+
+    const inputText = this.state.input
+
+    const wishList = this.props.wishList;
+    const indexOfItem = wishList.indexOf(inputText);
+
+    if (indexOfItem !== -1) {
+      this.setState({input: ""})
+      alert("Please do not repeat the item.");
+      return false;
+    }
+
+
+    if (inputText == "") {
+      alert("Please write something.");
+      return false;
+    }
+
+    this.props.addItem(inputText)
 
     this.setState({input: ""})
   }
@@ -24,6 +45,18 @@ class App extends React.Component {
     const item = e.target.textContent
 
     this.props.deleteItem(item)
+  }
+
+  handleSubmit(e) {
+
+    const wishList = this.props.wishList;
+
+    if (wishList == "") {
+      alert("WishList can't be empty")
+    } else {
+    alert('Wish list submitted to Santa!');
+    e.preventDefault();
+    }
   }
 
   render() {
@@ -38,17 +71,24 @@ class App extends React.Component {
 
     return(
       <div>
-        <h1> Wish List </h1>
-        <ul>
-          {listItems}
-        </ul>
-        <input
-          onChange={e => this.updateInput(e.target.value)}
-          value={this.state.input}
-        />
-        <button className="add-item" onClick={this.handleAddItem}>
-          Add Todo
-        </button>
+        <Container fluid>
+          <h1> Wish List </h1>
+          <ul id="list-items">
+            {listItems}
+          </ul>
+          <input
+            onChange={e => this.updateInput(e.target.value)}
+            value={this.state.input}
+          />
+          <Button variant="primary" className="add-item"
+            onClick={this.handleAddItem}>
+            {"Add Todo"}
+          </Button>
+          <Button variant="success" className="submit-list"
+            onClick={this.handleSubmit}>
+            Submit
+          </Button>
+        </Container>
       </div>
     );
   }
